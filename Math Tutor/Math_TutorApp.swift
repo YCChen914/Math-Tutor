922ae3ae1e2432a8a4b3b745ee2bd3dc6ecd9144
PaulChen <csz890914@gmail.com>
2023-08-16 15:11:26 +0800

    Initial Commit

---
+++ Math Tutor/Math_TutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Math_TutorApp.swift
+//  Math Tutor
+//
+//  Created by 陳永承 on 2023/8/15.
+//
+
+import SwiftUI
+
+@main
+struct Math_TutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
